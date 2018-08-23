3386172b7dc6d541a891f8ebbc1f3a0033318d93
Derek Smith and Julius Bade <julius@>
2018-08-23 11:03:35 -0700

    added function on restart button

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+$("#first").click(function(event) {
+ event.preventDefault();
+
+   $("#first").hide();
+ });
+
+
+
+
+
+
+// });
+// $("#refresh").click(function(){
+//   location.reload(true);
+
+
+
+
+$("#refresh").click(function(event) {
+location.reload(true);
+});
+});
