c192807bd73fba2f76568908c62be6afda5d2bb5
julius bade <julbade21@gmail.com>
2018-08-24 08:05:47 -0700

    added some files

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,124 @@
-function Player()
 
+//function Player()
 
+var placeholder = $("#placeholder");
+// var tictactoe = new TicTacToe(placeholder, 3 ,onResult);
+function onResult(result, scores) {
+
+}
 var playerOne = true
 var playerTwo = false
 
+
+// var space1 = $("#t1")
+// var space2 = $("#t2")
+// var space3 = $("#t3")
+// var space4 = $("#t4")
+// var space5 = $("#t5")
+// var space6 = $("#t6")
+// var space7 = $("#t7")
+// var space8 = $("#t8")
+// var space9 = $("#t9")
+
+
+// function TicTacToe(placeholder, callback, grid_size) {
+// 	this.placeholder = placeholder;
+//   this.paint = grid_size
+// 	this.callback = callback;
+// 	this.scores = {
+// 		X: 0,
+// 		O: 0
+// 	};
+// 	this.marks = {
+// 		X: "X",  // Player 1 mark
+// 		O: "O",  // Player 2 mark
+// 		count: 0 // Number of moves made in the whole game
+// 	};
+//
+// 	return this;
+// }
+// TicTacToe.prototype.paint = function(grid_size) {
+//   self.grid_size = grid_size;
+//   var html = '<table id="table" alignt="center">';
+//   for (var i = 0; i < grid_size; i++) {
+//     html += '<tr>';
+//     for( var j = 0; j < grid_size; j++) {
+//       html += '<td></td>';
+//     }
+//     html += '<tr>';
+//   }
+//   html += '<table>';
+//   self.placeholder.innerHTML = html;
+//   self.columns = self.placeholder.$("td");
+//   for(i = 0; 1 < this.columns.length; i++) {
+//     self.columns[i].addEventListener("click", markHandler);
+//   }
+//   function markHandler(e) {
+//     self.mark(e.target);
+//   }
+// };
+// TicTacToe.prototype.mark = function(column) {
+//   if(column.innerHTML) {
+//     return;
+//   } this.marks.count++;
+//   var currentMark = this.mark.count %2 === 1 ? this.marks.X : this.marks.O;
+//   column.innerHtml = currentMark;
+//   column.classList.add(currentMark);
+//
+//   if(this.playerWin(currentMark)) {
+//     if(this.marks.count % 2 === 1) {
+//       this.scores.X++;
+//     } else {
+//       this.scores.O++;
+//     }
+//     this.callback(currentMark, this.scores);
+//   } else if (this.marks.count === this.columns.length) {
+//     this.callback("draw");
+//   }
+// };
+// TicTacToe.prototype.playerWin = function(mark) {
+//   var grid_size = this.grid_size;
+//   var horizontalCount, verticalCount,rightToLeftCount = 0,
+//   leftToRightCount = 0;
+//   for(var i = 0; i < grid_size; i++) {
+// 		horizontal_count = vertical_count = 0;
+// 		for(var j = 0; j < grid_size; j++) {
+// 			if(this.columns[i * grid_size + j].innerHTML == mark) {
+// 				horizontal_count++;
+// 			}
+// 			if(this.columns[j * grid_size + i].innerHTML == mark) {
+// 				vertical_count++;
+// 			}
+// 		}
+//
+// 		if(horizontal_count == grid_size || vertical_count == grid_size) {
+// 			return true;
+// 		}
+//
+// 		if(this.columns[i * grid_size + i].innerHTML == mark) {
+// 			right_to_left_count++;
+// 		}
+// 		if(this.columns[(grid_size - 1) * (i+1)].innerHTML == mark) {
+// 			left_to_right_count++;
+// 		}
+// 	}
+// 	if(right_to_left_count == grid_size || left_to_right_count == grid_size) {
+// 		return true;
+// 	}
+//
+// 	return false;
+// };
+
+
+
+
+
 function switchTurn(){
   playerOne = !playerOne;
-  playerTwo = !playerTwo;
+  // playerTwo = !playerTwo;
 }
-//define w
-//define grid
-//if win show!!!
-//if tie show!!!
-var winningCombo = [["#t1","#t2","#t3"], ["#t1","#t4","#t7"], ["#t1","#t5","#t9"], ["#t2","#t5","#t8"], ["#t3","#t5","#t7"], ["#t3","#t6","#t9"],
- ["#t4","#t5","#t6"], ["#t7","#t8","#t9"]];
+
+
 
 
 
@@ -23,21 +128,20 @@ $("td").click(function(event) {
  event.preventDefault();
 $(this).css("background-image", "url('img/o.png')");
 $(this).css("background-size", "contain");
-console.log();
 
-
-if (playerOne === true) {
+if (playerOne == true) {
 
   $(this).css("background-image", "url('img/x.png')");
   $(this).css("background-size", "contain");
   switchTurn();
 
-} else {
+
+} else  {
   $(this).css("background-image", "url('img/o.png')");
   $(this).css("background-size", "contain");
   switchTurn();
 
-}
+};
 
 
 
